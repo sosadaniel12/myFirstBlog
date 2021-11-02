1f80a5dada79ee66d1fc0e86d8a9718e88437c33
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-11-02 11:45:35 -0500

    adding some boilerplate

--- server/server.js
+++ server/server.js
@@ -1,24 +1,30 @@
-const path = require("path");
 const express = require("express");
-const exphbs = require("express-handlebars");
 const routes = require("./controllers");
-const helpers = require("./utils/helpers");
 const sequelize = require("./config/connection");
-
+const path = require("path");
+const helpers = require("./utils/helpers");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const hbs = exphbs.create({ helpers });
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sess = {
+  secret: "Super secret secret",
+  cookie: { maxAge: 36000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 app.use(routes);
-
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
