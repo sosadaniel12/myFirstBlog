1e48e239ca8dcbddbf57e3331b67db6feb57f9bf
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-11-02 19:12:01 -0500

    pushing up some fixes

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -1,4 +1,4 @@
-const { User } = require("../models");
+const { User } = require("../server/models");
 
 const userData = [
   {
--- server/controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const userRoutes = require("./userRoutes.js");
-const postRoutes = require("./postRoutes");
-const commentRoutes = require("./commentRoutes");
-router.use("/users", userRoutes);
-router.use("/posts", postRoutes);
-router.use("/comments", commentRoutes);
-
-module.exports = router;
