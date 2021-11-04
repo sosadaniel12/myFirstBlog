954b16150534dd287fd00e7b6398f99c499463d2
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-11-03 22:56:26 -0500

    heroku

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User, Post, Comment } = require("../../models/index");
+const { User, Post, Comment } = require("../../models");
 router.get("/", (req, res) => {
   User.findAll({
     attributes: { exclude: ["[password"] },
